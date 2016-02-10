524efa6a58dc506feff2942c1a6c58b301521859
est77 <ramenhdr@gmail.com>
2016-02-10 18:26:10 +0100

    wip

--- hook/context_picker.py
+++ hook/context_picker.py
@@ -2,22 +2,20 @@ import ftrack
 import sys
 import os
 
+
 def register(registry, **kwargs):
     if registry is not ftrack.EVENT_HANDLERS:
         return
 
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
-    interfaces_path = os.path.abspath('../resources/interfaces')
-    resources_maya_path = os.path.abspath('../resources/maya')
-    source_code = os.path.abspath('../source')
+    this_dir = os.path.abspath(os.path.dirname(__file__))
+    interfaces_path = os.path.join(this_dir, "..", "resources", "interfaces")
+    maya_resources_path = os.path.join(this_dir, "..", "resources", "maya")
+    source_path = os.path.join(this_dir, '..', 'source')
 
     old_python_path = os.environ.get('PYTHONPATH', '')
-    os.environ['PYTHONPATH'] = os.pathsep.join([old_python_path, interfaces_path, resources_maya_path, source_code])
+    os.environ['PYTHONPATH'] = os.pathsep.join([old_python_path, interfaces_path, maya_resources_path, source_path])
 
     old_maya_script_path = os.environ.get('MAYA_SCRIPT_PATH', '')
-    os.environ['MAYA_SCRIPT_PATH'] = os.pathsep.join([old_maya_script_path, resources_maya_path])
+    os.environ['MAYA_SCRIPT_PATH'] = os.pathsep.join([old_maya_script_path, maya_resources_path])
 
     os.environ['EFESTO_CONTEXT_IFACE'] = 'ftrack'
-
-
