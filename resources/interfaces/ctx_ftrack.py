eaa3f4cf6ab9551716b743d9ef13f496b416db34
est77 <ramenhdr@gmail.com>
2017-08-07 12:16:17 +0200

    Use FTRACK_CONTEXTID. Use format to build queries.

--- resources/interfaces/ctx_ftrack.py
+++ resources/interfaces/ctx_ftrack.py
@@ -16,20 +16,22 @@ class FtrackContextManager(ContextInterface):
 
     def get_root_context(self):
         import ftrack_api
-        id = os.getenv('FTRACK_TASKID', os.getenv('FTRACK_SHOTID'))
+        id = os.getenv('FTRACK_CONTEXTID', os.getenv('FTRACK_TASKID', os.getenv('FTRACK_SHOTID')))
         task = self.session.get('Context', id)
         hierarchy = [x['name'] for x in task['link']]
         return hierarchy
 
     def get_context_data(self, hierarchy):
         import ftrack_api
-        query_str = 'Context where name is ' + hierarchy[-1]
+        query_str = 'Context where name is "%s"' % hierarchy[-1]
 
         # Build a query string of the form:
-        # Context where name is h[0] and parent.name is h[1] and parent.parent.name is h[2] ...
+        # Context where name is "h[0]" and parent.name is "h[1]" and parent.parent.name is "h[2]" ...
         parent_select = 'parent'
         for parent in reversed(hierarchy[:-1]):
-            query_str += ' and ' + parent_select + '.name is ' + parent
+            query_str += ' and {0}.name is "{1}"'.format(
+                parent_select, parent
+            )
             parent_select += '.parent'
 
         return self.session.query(query_str).one()
@@ -39,7 +41,7 @@ class FtrackContextManager(ContextInterface):
 
         if not result:
             tasks = self.session.query(
-                'select name from Task where parent.id is ' + obj['id']).all()
+                'select name from Task where parent.id is {0}'.format(obj['id'])).all()
             result = [x['name'] for x in tasks]
 
         return result
@@ -59,8 +61,11 @@ class FtrackContextManager(ContextInterface):
 
         obj = self.get_context_data(hierarchy)
         if obj.get('context_type') == 'task':
+            # Update ftrack environment variables for the new context.
+            # In ftrack, all three env vars point to the current task.
+            os.environ['FTRACK_CONTEXTID'] = obj['id']
             os.environ['FTRACK_TASKID'] = obj['id']
-            os.environ['FTRACK_SHOTID'] = obj['parent']['id']
+            os.environ['FTRACK_SHOTID'] = obj['id']
 
         self.execute_cb(self.get_interface_name(), hierarchy, None)
 
