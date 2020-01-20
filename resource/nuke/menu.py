3663302c55e6151842ef2678e28c3090367c7f26
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-01-20 20:29:54 +0100

    conform names

--- resource/nuke/menu.py
+++ resource/nuke/menu.py
@@ -6,7 +6,7 @@ import nuke
 try:
     from ftrack_context_navigator.nuke_navigator import createContextPickerPanel, NukeContextPickerWidget
     paneMenu = nuke.menu('Pane')
-    paneMenu.addCommand('Context Picker', createContextPickerPanel)
+    paneMenu.addCommand('Context Navigator', createContextPickerPanel)
 except:
     import traceback
     print traceback.format_exc()
