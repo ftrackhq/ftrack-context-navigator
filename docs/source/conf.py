b328ded5db686c66042e9e234fa8cc0f51e60cbf
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-01-20 20:27:49 +0100

    code cleanup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,8 +27,6 @@ autodoc_mock_imports = [
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..', 'source')))
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..', '.eggs')))
-
 
 # -- General configuration ------------------------------------------------
 
